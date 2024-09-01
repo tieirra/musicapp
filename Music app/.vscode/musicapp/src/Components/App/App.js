8873e1173d6dd00677e182c15f2b4de44cd24075
Tieirra <167941085+Tieirra77@users.noreply.github.com>
2024-08-31 20:16:29 -0500

    adding my functions and more code

--- Music app/.vscode/musicapp/src/Components/App/App.js
+++ Music app/.vscode/musicapp/src/Components/App/App.js
@@ -23,35 +23,77 @@ this.updatePlaylistName=this.updatePlaylistName.bind(this)
 this.savePlaylist=this.savePlaylist.bind(this)
 this.removeTrackSearch=this.removeTrackSearch.bind(this)
 this.doThese=this.doThese.bind(this);
-};
+}
 
+search(term){
 
+Spotify.search(term).then (SearchResults => {
+this.setState({ SearchResults: SearchResults});
+
+});
+
+}
+
+addTrack(track){
+let tracks= this.state.PlaylistTracks;
+if(tracks.find(savedTrack => savedTrack.id === track.id)){
+return;
+}
+tracks.push(Track);
+this.setState({PlaylistTracks: tracks});
+}
+
+removeTrack(Track){
+  let tracks = this.state.PlaylistTracks;
+  let trackSearch = this.state.SearchResults;
+  tracks=tracks.filter(currentTrack => currentTrack.id !== track.id);
+  trackSearch.unshift(track);
+  this.setState({PlaylistTracks: tracks})
+}
+
+removeTrackSearch(Track){
+  let tracks= this.state.SearchResults;
+  tracks=tracks.filter(currentTrack => currentTrack.id !== track.id);
+  this.setState({SearchResults: tracks});
+
+}
+doThese(Track){
+  this.addTrack(track);
+  this.removeTrackSearch(track);
+}
+updatePlaylistName(name){
+  this.setState({updatePlaylistName: name}); 
+}
+
+savePlaylist(){
+  const trackUris = this.state.PlaylistTracks.map(track => track.uri);
+  Spotify.savePlaylist(this.state.PlaylistName,trackUris).then (() => {
+    this.setState ({
+      updatePlaylistName: "NewPlaylist" ,
+      PlaylistTracks: []
+    });
+  
+});
 
 }
 
 
 
-
-
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+   <h1>
+   <a href="http://localhost:3000">Musicophile</a>
+   </h1>
+   <div> className ="App">    
+   <SearchBar onSearch={this.seaech} />
+   <div className= "App-Playlist">
+   <SearchResults SearchResults = {this.state.SearchResults} onAdd={this.doThese}/>
+   
+   <Playlist PlaylistTracks={this.state.PlaylistTracks} onNameChange={this.updatePlaylistName} onRemove={this.removeTrack} onSave={this.savePlaylist} />
+   </div>
+   </div>
+   </div>
   );
 }
 
